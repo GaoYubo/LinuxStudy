e00d0ef4c4f53a75dcc89d7beb547ad2b16256e2
GaoYubo <g_yubo@163.com>
2019-12-10 09:42:30 +0800

    Makefile简单语法了解

---
+++ embedfire/1.hello_demo/hello.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(void)
+{
+    printf("Hello World!\n");
+    printf("This is a c programe.\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("output i = %d\n",i);
+    }
+    
+    return 0;
+}
---
+++ embedfire/1.hello_demo/hello.s
@@ -0,0 +1,45 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello World!"
+.LC1:
+	.string	"This is a c programe."
+.LC2:
+	.string	"output i = %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$.LC0, %edi
+	call	puts
+	movl	$.LC1, %edi
+	call	puts
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$4, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
