be32643d3ca0762968b76a334d2ae1a3cd75416e
GaoYubo <g_yubo@163.com>
2019-12-11 09:31:42 +0800

    结构化工程建立

---
+++ embedfire/3.makefile_plus/sources/func.c
@@ -0,0 +1,11 @@
+#include "func.h"
+
+void hello_func(void)
+{
+    printf("This is a c programe.\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("output i = %d\n",i);
+    }
+    
+}
---
+++ embedfire/3.makefile_plus/sources/hello.c
@@ -0,0 +1,9 @@
+#include "func.h"
+
+int main(void)
+{
+    printf("Hello World!\n");
+    hello_func();
+
+    return 0;
+}
